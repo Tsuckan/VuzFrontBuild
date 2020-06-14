62d652d2715914eb5c28a7e3a3cfb6f42cd6722d
Tsuckan <48693147+Tsuckan@users.noreply.github.com>
2020-06-14 19:09:18 +0300

    Add files via upload

---
+++ build/precache-manifest.78d3de2ff880fc78819af7aeab0af02b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf43c6da75e61b50e362bff02e2de77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5566c5ab7c9d47af433",
+    "url": "/static/css/main.1180b480.chunk.css"
+  },
+  {
+    "revision": "c4b710be155f1bd381fa",
+    "url": "/static/js/2.7b9440be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b9440be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5566c5ab7c9d47af433",
+    "url": "/static/js/main.4584131d.chunk.js"
+  },
+  {
+    "revision": "eecd364fcf183009ba87",
+    "url": "/static/js/runtime-main.d25a7209.js"
+  }
+]);
